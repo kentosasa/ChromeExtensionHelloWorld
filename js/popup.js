77716b3f345583f802237ad5d05bea9f83c3bd29
kento sasamoto <knote0721@gmail.com>
2015-06-08 18:26:19 +0900

    add popup

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,16 @@
-$(function () {
-  chrome.tabs.getSelected(null, function(tab) {
-    $('#title').text(tab.title);
-    $('#url').text(tab.url);
-  });
+//空のデータを用意する
+var data = [];
+
+//値の呼び出し
+chrome.storage.sync.get('histories', function(items){
+  data = data.concat(items.histories);
+  console.log("popup");
+  console.log(data);
+  for(var i = 0; i < data.length; i++){
+    var history = "<p><a href=\"" + data[i].url +"\" >" + data[i].title + "</a></p>";
+
+    $("body").append(history);
+  }
 });
-console.log("変更したよ");
+
+
