c8b75852931a9d911dab29d4ecfdd4b4299f8b86
kento sasamoto <knote0721@gmail.com>
2015-06-15 19:11:27 +0900

    sample react

---
+++ jsx/app.js
@@ -0,0 +1,16 @@
+console.log("call app.js");
+
+var CommentBox = React.createClass({
+  render: function(){
+    return (
+        <div className="commentBox">
+          Hello, world I am a CommentBox.
+        </div>
+        );
+  }
+});
+
+React.render(
+    <CommentBox />,
+    document.getElementById('content')
+    );
