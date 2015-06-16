7d9430233e30f3ff445321f5befbe09868e7b948
kento sasamoto <knote0721@gmail.com>
2015-06-16 16:13:34 +0900

    add comment

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 //空データ
 var items = [];
-items.push({name: "hoge", hoge: "hoge"});
 
+
+//ReactでViewの元となるHistoryListClassを作成する
 var HistoryList = React.createClass({displayName: "HistoryList",
   render: function(){
     var historyItems = this.props.items.map(function(item){
       var date = new Date(item.date);
+      console.log(date.getDay()+1);
       return(
           React.createElement("div", {className: "history"}, 
             React.createElement("p", null, date.getMonth()+1, "月", date.getDay()+1, "日 ", React.createElement("a", {target: "_blank", href: item.url}, item.title))
@@ -20,16 +22,17 @@ var HistoryList = React.createClass({displayName: "HistoryList",
   }
 });
 
-
+/* HistoryListにitemを渡して、contentの位置に追加する
 React.render(
-    React.createElement(HistoryList, {items: items}),
+    <HistoryList items={items}/>,
     document.getElementById('content')
     );
-
+*/
 
 //値の呼び出し
 chrome.storage.local.get('histories', function(data){
   items = items.concat(data.histories);
+  //データが変わったので更新を行う
   React.render(
     React.createElement(HistoryList, {items: items}),
     document.getElementById('content')
--- jsx/app.js
+++ jsx/app.js
@@ -1,11 +1,13 @@
 //空データ
 var items = [];
-items.push({name: "hoge", hoge: "hoge"});
 
+
+//ReactでViewの元となるHistoryListClassを作成する
 var HistoryList = React.createClass({
   render: function(){
     var historyItems = this.props.items.map(function(item){
       var date = new Date(item.date);
+      console.log(date.getDay()+1);
       return(
           <div className="history">
             <p>{date.getMonth()+1}月{date.getDay()+1}日 <a target="_blank" href={item.url}>{item.title}</a></p>
@@ -20,16 +22,17 @@ var HistoryList = React.createClass({
   }
 });
 
-
+/* HistoryListにitemを渡して、contentの位置に追加する
 React.render(
     <HistoryList items={items}/>,
     document.getElementById('content')
     );
-
+*/
 
 //値の呼び出し
 chrome.storage.local.get('histories', function(data){
   items = items.concat(data.histories);
+  //データが変わったので更新を行う
   React.render(
     <HistoryList items={items}/>,
     document.getElementById('content')
